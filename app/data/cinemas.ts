54fbbaed41a0c18f37f631ea00fd0882ff0261e6
Victor Vergoossen <victor.vergoossen@dephion.com>
2022-07-23 16:02:16 +0200

    Changed cinema name

--- app/data/cinemas.ts
+++ app/data/cinemas.ts
@@ -1,7 +1,7 @@
 // Enum matching ID to place from the RSS feed
 export const Cinemas: Record<string, number> = { // TODO: Extend to feature all required cinemas
   Echt: 45,
-  Deventer: 21,
+  Boxmeer: 21,
   Breda: 22,
   Brielle: 23,
 };
