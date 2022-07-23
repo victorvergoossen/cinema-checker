f81f02f0a09e528a1f24a2b061c920b58e2bfc17
Victor Vergoossen <victor.vergoossen@dephion.com>
2022-07-23 15:19:01 +0200

    removed build folders

--- ios/Pods/Target Support Files/Pods-CollectionManager-CollectionManagerTests/Pods-CollectionManager-CollectionManagerTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CollectionManager_CollectionManagerTests : NSObject
-@end
-@implementation PodsDummy_Pods_CollectionManager_CollectionManagerTests
-@end
--- ios/Pods/Target Support Files/Pods-CollectionManager-tvOSTests/Pods-CollectionManager-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CollectionManager_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_CollectionManager_tvOSTests
-@end
